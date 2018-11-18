819728c91c02cff2beb18330f356c70e63f2d566
Topi62 <toivo.karkinen@helsinki.fi>
2018-11-18 13:24:11 +0200

    viikon 3 tehtävät kolmas

---
+++ LoginCucumber/src/main/java/ohtu/services/AuthenticationService.java
@@ -0,0 +1,60 @@
+package ohtu.services;
+
+import static java.lang.Character.isLetter;
+import ohtu.domain.User;
+import java.util.ArrayList;
+import java.util.List;
+import ohtu.data_access.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticationService {
+
+    private UserDao userDao;
+    
+    @Autowired
+    public AuthenticationService(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public boolean logIn(String username, String password) {
+        for (User user : userDao.listAll()) {
+            if (user.getUsername().equals(username)
+                    && user.getPassword().equals(password)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean createUser(String username, String password) {
+        if (userDao.findByName(username) != null) {
+            return false;
+        }
+
+        if (invalid(username, password)) {
+            return false;
+        }
+
+        userDao.add(new User(username, password));
+
+        return true;
+    }
+
+    private boolean invalid(String username, String password) {
+        // validity check of username and password
+        boolean letter=true;
+        if (username.length()<3 || password.length()<8){
+            return true;
+        }
+        for (int x = 0;x <password.length(); x++){
+        if (isLetter(password.charAt(x))==false){
+            letter=false;
+        }
+        }
+
+        return letter;
+    }
+}
